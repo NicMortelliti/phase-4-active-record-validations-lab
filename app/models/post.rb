3e54a5164e3f7c01d4f9d1aa5142d9e84f1992f7
NicMortelliti <npmortelliti@gmail.com>
2022-08-01 14:25:09 -0700

    commit tree 5f2b66feeb558e76a80de43be2cca57ecb798ef2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :must_be_click_batey
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def must_be_click_batey
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add( :title, 'must be clickbait' )
+    end
+  end
 end
